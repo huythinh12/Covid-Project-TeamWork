981161bee20095779589e16ff59cbdf271a831c3
FinnNIP <dddung487@gmail.com>
2021-08-16 22:16:24 +0700

    Add Left & Right Animation
    Thêm animation trái và phải

--- Assets/Script/P_AnimController.cs
+++ Assets/Script/P_AnimController.cs
@@ -10,14 +10,25 @@ namespace VN
         private Animator _Panim;
 
         private int _IsWalkingHash;
+
+        private int _IsWalkLeftHash;
+        private int _IsWalkRightHash;
+
         private int _IsRunningHash;
         private int _IsDancingHash;
+
+
 
         // Start is called before the first frame update
         void Start()
         {
             _Panim = GetComponent<Animator>();
+
             _IsWalkingHash = Animator.StringToHash("IsWalk");
+
+            _IsWalkLeftHash = Animator.StringToHash("IsWalkLeft");
+            _IsWalkRightHash = Animator.StringToHash("IsWalkRight");
+
             _IsRunningHash = Animator.StringToHash("IsRun");
             _IsDancingHash = Animator.StringToHash("IsDance");
 
@@ -31,7 +42,22 @@ namespace VN
 
         private void PMovement()
         {
+
+            bool _IsWalk = _Panim.GetBool("IsWalk");
+
+            bool _forwardPressed = Input.GetKey("w");
+
+            bool _IsLeftPressed = Input.GetKey("a");
+
             bool _DancePress = Input.GetKey("space");
+
+            bool _IsRightPressed = Input.GetKey("d");
+
+            bool _IsRun = _Panim.GetBool("IsRun");
+            bool _runPressed = Input.GetKey("left shift");
+
+
+
 
             if (_DancePress)
             {
@@ -43,21 +69,45 @@ namespace VN
             }
 
 
-
-            bool _IsWalk = _Panim.GetBool("IsWalk");
-            bool _IsRun = _Panim.GetBool("IsRun");
-            bool _forwardPressed = Input.GetKey("w");
-            bool _runPressed = Input.GetKey("left shift");
+           
 
             if (!_IsWalk && _forwardPressed)
+                
             {
                 _Panim.SetBool(_IsWalkingHash, true);
             }
+            else
             if (_IsWalk && !_forwardPressed)
             {
                 _Panim.SetBool(_IsWalkingHash, false);
             }
+            //bool _IsWalkLeft = _Panim.GetBool("IsWalkLeft");
+            else
+            if (_IsLeftPressed || _forwardPressed && _IsLeftPressed)
+            {
+                _Panim.SetBool(_IsWalkLeftHash, true);
+            }
+            else
+            if (!_IsLeftPressed)
+            {
+                _Panim.SetBool(_IsWalkLeftHash, false);
+            }
+            //bool _IsWalkRight = _Panim.GetBool("IsWalkRight");
+            
+            if ( _IsRightPressed || _forwardPressed && _IsRightPressed)
+            {
+                Debug.Log("Right");
+                _Panim.SetBool(_IsWalkRightHash, true);
+                
+            }
+            else
+            if (!_IsRightPressed)
+            {
+                _Panim.SetBool(_IsWalkRightHash, false);
+            }            
 
+
+           
             if (!_IsRun && (_forwardPressed && _runPressed))
             {
                 _Panim.SetBool(_IsRunningHash, true);
